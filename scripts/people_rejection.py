ea33c6e8ad0f729ba54359c5a26c6426df32fc05
Nils Bore <nilsbore@gmail.com>
2016-11-03 12:01:46 +0100

    Added hakans detection node

--- scripts/people_rejection.py
+++ scripts/people_rejection.py
@@ -1,21 +1,48 @@
 #!/usr/bin/python
 
 import rospy
-from sensor_msgs.msg import CameraInfo
+from sensor_msgs.msg import CameraInfo, Image
 from semantic_map.msg import RoomObservation
 from upper_body_detector.msg import UpperBodyDetector
 from os import path
 import json
+from deep_object_detection.msg import Object
+from deep_object_detection.srv import DetectObject
 
 class RejectionServer(object):
 
     def observation_cb(self, msg):
 
         self.sweep_detections.append(latest_detections)
+        self.images.append(msg)
 
     def finished_cb(self, msg):
 
         sweep_dir = path.join(path.abspath(msg.xml_file_name), path.pardir)
+
+        req = DetectObjectRequest()
+        req.images = self.images
+        rospy.wait_for_service('/deep_object_detection/detect_objects')
+        try:
+            server = rospy.ServiceProxy('/deep_object_detection/detect_objects', DetectObject)
+            resp = server(req)
+        except rospy.ServiceException, e:
+            print "Service call failed: %s"%e
+            return
+
+        detections = [[] for im in self.images]
+        for obj in resp.objects:
+            if obj.label != "chair":
+                continue
+            vals = {'x': obj.x, 'y': obj.y, 'width': obj.width, 'height': obj.height}
+            detections[obj.imageID].append(vals)
+
+        for ind, vec in enumerate(detections):
+            name = "intermediate_deep_detection%4d.json" % ind
+            filename = path.join(sweep_dir, name)
+            with open(filename, 'w') as outfile:
+                json.dump(vec, outfile)
+
         for ind, det in enumerate(self.sweep_detections):
             name = "intermediate_detection%4d.json" % ind
             filename = path.join(sweep_dir, name)
@@ -33,12 +60,13 @@ class RejectionServer(object):
 
     def __init__(self):
 
-        self.observation_sub = rospy.Subscriber('/local_metric_map/depth/camera_info', CameraInfo, callback=self.observation_cb)
+        self.observation_sub = rospy.Subscriber('/local_metric_map/rgb/rgb_filtered', Image, callback=self.observation_cb)
         self.finished_sub = rospy.Subscriber('/local_metric_map/room_observations', RoomObservation, callback=self.finished_cb)
         self.detection_sub = rospy.Subscriber('/upper_body_detector/detections', UpperBodyDetector, callback=self.detection_cb)
 
         self.latest_detections = UpperBodyDetector()
         self.sweep_detections = []
+        self.images = []
 
 if __name__ == '__main__':
 
